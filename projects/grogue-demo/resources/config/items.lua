db0861cc67b6200d6cc81edaf0f0b1be32577fab
VisualGMQ <2142587070@qq.com>
2023-06-08 17:12:54 +0800

    "publish"

---
+++ projects/grogue-demo/resources/config/items.lua
@@ -0,0 +1,96 @@
+Config = {
+    totle_weight = 20,
+
+    items = {
+        stone = {
+            name = "石头",
+            operations = {"drop", "use"},
+            weight = 2,
+            sprite = {
+                color = {255, 255, 255, 255},
+                tilesheet = {
+                    name = "tilesheet",
+                    row = 9,
+                    col = 2,
+                },
+            },
+            material = {
+                opaque = 1.0,
+                solid = 1.0,
+                lumen = 0.0,
+            },
+        },
+        big_stone = {
+            name = "大石头",
+            operations = {"drop", "use"},
+            weight = 4,
+            sprite = {
+                color = {255, 255, 255, 255},
+                tilesheet = {
+                    name = "tilesheet",
+                    row = 9,
+                    col = 1,
+                },
+            },
+            material = {
+                opaque = 1.0,
+                solid = 1.0,
+                lumen = 0.0,
+            },
+        },
+        wooden_fork = {
+            name = "树枝",
+            operations = {"drop", "use"},
+            weight = 4,
+            sprite = {
+                color = {255, 255, 255, 255},
+                tilesheet = {
+                    name = "tilesheet",
+                    row = 9,
+                    col = 0,
+                },
+            },
+            material = {
+                opaque = 1.0,
+                solid = 1.0,
+                lumen = 0.0,
+            },
+        },
+        sharp_stone = {
+            name = "锋利石头",
+            operations = {"drop", "use"},
+            weight = 2,
+            sprite = {
+                color = {255, 255, 255, 255},
+                tilesheet = {
+                    name = "tilesheet",
+                    row = 9,
+                    col = 3,
+                },
+            },
+            material = {
+                opaque = 1.0,
+                solid = 1.0,
+                lumen = 0.0,
+            },
+        },
+        stake = {
+            name = "木桩",
+            operations = {"drop", "use"},
+            weight = 2,
+            sprite = {
+                color = {255, 255, 255, 255},
+                tilesheet = {
+                    name = "tilesheet",
+                    row = 9,
+                    col = 4,
+                },
+            },
+            material = {
+                opaque = 1.0,
+                solid = 1.0,
+                lumen = 0.0,
+            },
+        },
+    }
+}
